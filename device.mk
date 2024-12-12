55bc8328f034e20784963798fc8c66d5ed0c9d38
Alcatraz323 <alcatraz32323@gmail.com>
2024-12-12 21:07:07 +0800

    umi: Implement DSPVolumeSynchronizer
    * umi also needs this to handle bass dynamically
    
    Change-Id: I750803d94161e1e7482552d2a39566f42e82fc0a

--- device.mk
+++ device.mk
@@ -32,6 +32,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,qdcm_calib_data_*.xml,$(LOCAL_PATH)/qdcm/,$(TARGET_COPY_OUT_VENDOR)/etc/)
 
+# DSP Volume Synchronizer
+PRODUCT_PACKAGES += \
+    DSPVolumeSynchronizer
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
