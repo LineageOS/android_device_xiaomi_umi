0c0af3ae2ed1964b332e170e9bc8d23c2278b0a9
Flicker372 <flicker372@outlook.com>
2025-07-08 20:42:00 +0800

    umi: Use simpler powershare path
    * Fix SystemUI crash
    * Soong function can't accept ","
    
    Co-authored-by: Alcatraz323 <alcatraz32323@gmail.com>
    Change-Id: I16f9ec2094575b63f77556f6a12d69428bb29e37

--- device.mk
+++ device.mk
@@ -51,7 +51,7 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_PACKAGES += \
     vendor.lineage.powershare-service.default
 
-$(call soong_config_set,lineage_powershare,powershare_path,/sys/devices/platform/soc/c440000.qcom,spmi/spmi-0/spmi0-02/c440000.qcom,spmi:qcom,pm8150b@2:qcom,qpnp-smb5/power_supply/wireless/reverse_chg_mode)
+$(call soong_config_set,lineage_powershare,powershare_path,/sys/class/power_supply/wireless/reverse_chg_mode)
 
 # Rootdir
 PRODUCT_PACKAGES += \
